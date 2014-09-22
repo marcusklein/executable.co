3bd429a6d986b693b9b699fd1fd6d912753c3860
marcusklein <marcusklein@me.com>
2014-09-23 01:53:34 +1200

    nightly commit

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // Changes the panel sizes to dynamically fit browser window
 function fireContentResizer() {
-	console.log("fired resize");
 	document.body.style.overflow = "hidden";
 	var numPanels = 8;
 	var nh = $(window).height();
 	var nw = $(window).width();
-	$("#mainTitle").center(nw, nh);
+	console.log(nw);
+	center("#mainTitle", nw, nh);
+	center("#navbar", nw, nh);
 	document.body.style.overflow = "";
 	for (var i = 1; i <= numPanels; i++) {
 		$("#panel_" + i).height(nh);
 		$("#panel_" + i).width(nw);
 		$("#panel_" + i).addClass("panelBreak");
 	};
-
-
-
 };
+
+function center(target, nw, nh) {
+	if ($(target).attr("id") !== "navbar")
+		$(target).css("top", (nh / 2) - ($(target).height() / 2) + "px");
+	$(target).css("left", (nw / 2) - ($(target).width() / 2) + "px");
+}
 
 function loadNiceVideo() {
 	$("#panel_1").append("<video id=\"bgNiceVid\" autoplay=\"autoplay\" loop><source src=\"resources/water2.mp4\" type=\"video/mp4\"></video>");
@@ -33,23 +37,45 @@ function getScrollbarWidth() {
 	return vpWidthNoOverflow - vpWidthOverflow;
 }
 
+		function detectmob() { 
+			if( navigator.userAgent.match(/Android/i)
+				|| navigator.userAgent.match(/webOS/i)
+				|| navigator.userAgent.match(/iPhone/i)
+				|| navigator.userAgent.match(/iPad/i)
+				|| navigator.userAgent.match(/iPod/i)
+				|| navigator.userAgent.match(/BlackBerry/i)
+				|| navigator.userAgent.match(/Windows Phone/i)) {
+				return true;
+		}else {
+			return false;
+		}
+	}
+
 
 $(document).ready(function () {
 
-	// Preload the page
-	$(window).load(function(){
-		$('#preloader').fadeOut('slow',function(){$(this).remove();});
-	});
+		// Preload the page
+		$(window).load(function(){
+			$('#preloader').fadeOut('slow',function(){$(this).remove();});
+		});
 
-	// Load the better video
-	loadNiceVideo();
-	$("#bgNiceVid").load($("#bgVid").fadeOut('slow',function(){
-		$(window).resize();
-		$(this).remove();
-	}));
+	if(detectmob()) {
+		$("#video_background").remove();
+		$("#bgNiceVid").remove();
+	} else {
+		// Load the better video
+		// Getting creative to fight the ipad problem
+		loadNiceVideo();
+		$("#bgNiceVid").load($("#bgVid").fadeOut('slow',function(){
+			$(window).resize(); // Because chrome sucks
+			$(this).remove();
+		}));
+
+	}
 
 	// Smooth scrolling
 	$("nav").click(function(event) {
+		event.preventDefault();
 		var panelNum = event.target.id;
 		panelNum = panelNum.replace(/\D/g,'');
 		$('html, body').animate({
@@ -65,13 +91,3 @@ $(document).ready(function () {
 $(window).resize(function() {
 	fireContentResizer();
 });
-
-
-jQuery.fn.center = function (nw, nh) {
-	this.css("position","absolute");
-	this.css("top", "50%");
-	this.css("left", (nw / 2) - ($(this).width() / 2) + "px");
-	//this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft() - getScrollbarWidth()) + "px");
-	return this;
-}
-
